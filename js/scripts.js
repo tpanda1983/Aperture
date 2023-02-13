5e35683dfd033c1e6c16dcfcfe5b241bff5897f0
tpanda1983 <tpandatpanda1983@gmail.com>
2023-02-13 21:08:51 +0200

    new 2

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+        autoplay:true,
+        autoplayTimeout:2000,
+        responsive:{
+            0:{
+                items:2
+            },
+
+            1000:{
+                items:5
+            }
+        }
+    });
+    $( function() {
+        $( "#dialog" ).dialog({
+            autoOpen: false,
+            show: {
+                effect: "blind",
+                duration: 1000
+            },
+            hide: {
+                effect: "explode",
+                duration: 1000
+            }
+        });
+
+        $( "#opener" ).on( "click", function() {
+            $( "#dialog" ).dialog( "open" );
+        });
+    } );
+});
